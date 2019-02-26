626f45a8499764ac65725eea902678ee244dd679
omkarstpt <omkarstpt@gmail.com>
2019-02-25 22:12:22 -0600

    Added category of expense

--- js/dateValidate.js
+++ js/dateValidate.js
@@ -1,25 +1,43 @@
 function validateForm() {
     var x = document.getElementById('date').value;
     var y = document.getElementById('c2').value;
+    var z = document.getElementById('inputState').value;
     if (x == "") {
       document.getElementById("date-div").classList.add('has-error');
       document.getElementById('date-help').style.visibility = "visible";
       if (y == "0") {
         document.getElementById("expense-amount-div").classList.add('has-error');
         document.getElementById('expense-amount-help').style.visibility = "visible";
+        document.getElementById('expense-label').style.color = "brown";
+      }
+      if (z == "Choose...") {
+        document.getElementById("category-div").classList.add('has-error');
+        document.getElementById('category-help').style.visibility = "visible";
+        document.getElementById('category-label').style.color = "brown";
       }
       return false;
     }
     if (y == "0") {
       document.getElementById("expense-amount-div").classList.add('has-error');
       document.getElementById('expense-amount-help').style.visibility = "visible";
+      document.getElementById('expense-label').style.color = "brown";
       if (x == "") {
         document.getElementById("date-div").classList.add('has-error');
         document.getElementById('date-help').style.visibility = "visible";
       }
+      if (z == "Choose...") {
+        document.getElementById("category-div").classList.add('has-error');
+        document.getElementById('category-help').style.visibility = "visible";
+        document.getElementById('category-label').style.color = "brown";
+      }
       return false;
     }
-    document.getElementById("login-button").onclick = function() {
-      window.location("page3.html")
+
+    if (z == "Choose...") {
+      document.getElementById("category-div").classList.add('has-error');
+      document.getElementById('category-help').style.visibility = "visible";
+      document.getElementById('category-label').style.color = "brown";
+
+      return false;
     }
   }
