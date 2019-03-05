2a0365ca6ee8825195e0217d0739d7bc2814af63
omkarstpt <omkarstpt@gmail.com>
2019-03-05 13:15:25 -0600

    Added signup

---
+++ js/signupform.js
@@ -0,0 +1,29 @@
+function validateForm() {
+    var x = document.getElementById('login-username').value;
+    var y = document.getElementById('login-pass').value;
+    var z = document.getElementById('login-pass2').value;
+    var e = document.getElementById('login-email').value;
+    
+    if (x == "") {
+      document.getElementById("username-div").classList.add('has-error');
+      document.getElementById('username-help').style.visibility = "visible";
+    }
+    if (y == "") {
+    document.getElementById("pass-div").classList.add('has-error');
+    document.getElementById('password-help').style.visibility = "visible";
+    }
+    if (z == "") {
+        document.getElementById("pass2-div").classList.add('has-error');
+        document.getElementById('password2-help').style.visibility = "visible";
+    }
+    if (e == "") {
+        document.getElementById("email-div").classList.add('has-error');
+        document.getElementById('email-help').style.visibility = "visible";
+    }
+    
+    
+    if (x == "" || y == "" || z == "" || e == "" ){
+        return false;
+    }
+    
+  }
